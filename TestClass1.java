8435b249fd6e578d120006febac5cc25b04f1763
Rutu204 <109073198+Rutu204@users.noreply.github.com>
2022-07-25 13:57:58 +0530

    Add files via upload

---
+++ TestClass1.java
@@ -0,0 +1,9 @@
+public class TestClass1
+{
+	int y = 8;
+	public static void main(String arg[])
+	{
+		TestClass1 myObj = new TestClass1();
+		System.out.println("y:" + myObj.y);
+	}
+}
---
+++ TestClass2.java
@@ -0,0 +1,9 @@
+public class TestClass2 
+{
+	int z = 7;
+	public static void main(String arg[])
+	{
+		TestClass2 myObj = new TestClass2();
+		System.out.println("z:" + myObj.z);
+	}
+}
